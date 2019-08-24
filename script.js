37f3182cbba1fc81cc6d659fedf66f59e4cbd5aa
alizaLakhani14 <49196299+alizaLakhani14@users.noreply.github.com>
2019-08-24 18:05:15 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+// API : Application Programming Interface(set of URLs to communicate with the Backend).
+// Static: Aapne hae cheez hardcode kardi.Agar kuch change karna hai tou har dafa code update karna hai.
+// Dynamic : khud ba khud upsdate honay wali cheez ko dynamic kehtay hain.
+// www.faebook.com (Server ka address)
+// www.facebook.com/messenger(messenger = path)
+//  www.facebook.com/messenger?(Question mark aagay jo bh likha hai usay query string kehtay hain).
+/*
+https://github.com/search?q=somia+ansari
+protocol : https
+server address : www.github.com
+path:/search
+queryString:q=somia+ansari
+
+https://www.google.com/search?hl=en&
+ei=PQlhXfb4LY_4wALXlZioBQ&
+q=biryani&
+oq=biryani&
+gs_l=psy-ab.3..0i67l5j0l5.27995.30414..31046...0.1..0.368.1719.0j6j0j2......0....1..gws-wiz.....0..0i71j0i131.QGq8pfuTPU8
+&ved=0ahUKEwi2i4q6npvkAhUPPFAKHdcKBlUQ4dUDCAo&
+uact=5
+*/
+
+function getWeather(){
+    let cityName = document.querySelector("input").value;
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=50c26845238d8cdbe5c3acddbf4ffd1b`)
+    .then(function (response) {
+      // handle success
+      document.querySelector("h1").innerHTML = response.data.weather[0].main;
+    //   console.log(response.data.weather[0].main );
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .finally(function () {
+      // always executed
+    });
+}
+
+
+
+
